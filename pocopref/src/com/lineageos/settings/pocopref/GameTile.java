9d2d9a9529a1869e2f138d4b81dce1f12970f7ab
shivatejapeddi <shivatejapeddi@gmail.com>
2020-06-02 21:25:56 +0530

    sdm845-common : Add KCAL Calibration, Eyecare Mode, Enhanced Display tile and Refactor Code *ENhanced display tile works with Mod

---
+++ pocopref/src/com/lineageos/settings/pocopref/GameTile.java
@@ -0,0 +1,115 @@
+/*
+ *  Poco Extras Settings Module
+ *  Made by @shivatejapeddi 2019
+ */
+
+package com.lineageos.settings.pocopref;
+
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import com.lineageos.settings.pocopref.XiaomiDisplay;
+import com.lineageos.settings.pocopref.utils.Shell;
+import com.lineageos.settings.pocopref.R;
+import java.util.ArrayList;
+import java.util.List;
+import android.util.Log;
+
+/*
+ * Based on the original implementation of Spectrum Kernel Manager by frap129 <joe@frap129.org>
+ *
+ * Originally authored by Morogoku <morogoku@hotmail.com>
+ *
+ * Modified by sunilpaulmathew <sunil.kde@gmail.com>
+ */
+
+public class GameTile extends TileService {
+
+    private int id = 0;
+    private static final String TAG = Shell.class.getSimpleName();
+
+    @Override
+    public void onStartListening() {
+        resetTileStatus();
+    }
+
+    @Override
+    public void onClick() {
+        updateTile();
+    }
+
+    private void updateTile() {
+        Tile tile = this.getQsTile();
+        Icon newIcon;
+	newIcon =  Icon.createWithResource(getApplicationContext(), R.drawable.ic_enhanced_display);
+        String newLabel;
+        int newState;
+
+        if (id == 0) {
+		newLabel = "Default";
+		newState = Tile.STATE_INACTIVE;
+		id +=1;
+            runCommand("display ENHANCE 1");
+            runCommand("display ADAPT 3");
+            } else if (id == 1) {
+		newLabel = "Bright";
+		newState = Tile.STATE_ACTIVE;
+		id +=1;
+            runCommand("display GAME_HDR 2");
+            } else if (id == 2) {
+		newLabel = "Saturation";
+		newState = Tile.STATE_ACTIVE;
+		id +=1;
+            runCommand("display GAME_HDR -38");
+            } else {
+		newLabel = "Bri+Sat";
+		newState = Tile.STATE_ACTIVE;
+		id = 0;
+            runCommand("display GAME_HDR 41");
+            }
+
+        // Change the UI of the tile.
+        tile.setLabel(newLabel);
+        tile.setIcon(newIcon);
+        tile.setState(newState);
+        tile.updateTile();
+    }
+
+   public static void runCommand(String command) {
+        Log.d(TAG, "setCurrentProfile() cmd=\'" + command + "\'");
+        ArrayList<String> output = Shell.runWithShell(command);
+        for (String line : output) {
+            Log.d(TAG, "setCurrentProfile() output:" + line);
+        }
+   }
+
+    private void resetTileStatus() {
+        int profile = 0;
+        Tile tile = this.getQsTile();
+        Icon newIcon;
+      	newIcon =  Icon.createWithResource(getApplicationContext(), R.drawable.ic_enhanced_display);
+        String newLabel;
+        int newState;
+
+        if (profile == 3) {
+		newLabel = "Saturation";
+		newState = Tile.STATE_ACTIVE;
+            } else if (profile == 2) {
+		newLabel = "Bright";
+		newState = Tile.STATE_ACTIVE;
+            } else if (profile == 1) {
+		newLabel = "Default";
+		newState = Tile.STATE_INACTIVE;
+            } else {
+		newLabel = "Bri+Sat";
+		newState = Tile.STATE_ACTIVE;
+            }
+
+        // Change the UI of the tile.
+        tile.setLabel(newLabel);
+        tile.setIcon(newIcon);
+        tile.setState(newState);
+        tile.updateTile();
+    }
+}
--- pocopref/src/com/lineageos/settings/pocopref/XiaomiDisplay.java
+++ pocopref/src/com/lineageos/settings/pocopref/XiaomiDisplay.java
@@ -27,8 +27,12 @@ import android.preference.ListPreference;
 import android.preference.SwitchPreference;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.view.MenuItem;
 import android.os.Bundle;
 import android.util.Log;
+
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import android.app.ActionBar;
 import com.lineageos.settings.pocopref.utils.Shell;
@@ -46,17 +50,44 @@ public class XiaomiDisplay extends PreferenceActivity implements
 
     private static final String XIAOMI_DISPLAY_PROFILE = "xiaomi_display_profile";
     private static final String PREF_XIAOMI_PROFILE = "xiaomi_profile";
-    private static final String PREF_GAME_PROFILE = "xiaomi_profile";
     private static final String DEFAULT_XIAOMI_PROFILE = "ADAPT 3";
-    public static final String COLOR_TEMP = "color_temp";
+
+    public static final String KEY_KCAL_RED = "kcal_red";
+    public static final String KEY_KCAL_GREEN = "kcal_green";
+    public static final String KEY_KCAL_BLUE = "kcal_blue";
+    public static final String KEY_KCAL_SATURATION = "kcal_saturation";
+    public static final String KEY_KCAL_CONTRAST = "kcal_contrast";
+    public static final String KEY_KCAL_HUE = "kcal_hue";
+    public static final String KEY_KCAL_VALUE = "kcal_value";
+    public static final String KEY_KCAL_EYECARE = "kcal_eyecare";
+
+    private SeekBarPreference mKcalRed;
+    private SeekBarPreference mKcalBlue;
+    private SeekBarPreference mKcalGreen;
+    private SeekBarPreference mKcalSaturation;
+    private SeekBarPreference mKcalContrast;
+    private SeekBarPreference mKcalHue;
+    private SeekBarPreference mKcalValue;
+    private SeekBarPreference mKcalEyecare;
+    private SharedPreferences mPrefs;
+    private boolean mEnabled;
+
+    private static final String COLOR_FILE = "/sys/module/msm_drm/parameters/";
+    private static final String COLOR_FILE_RED = COLOR_FILE + "/kcal_red";
+    private static final String COLOR_FILE_GREEN = COLOR_FILE + "/kcal_green";
+    private static final String COLOR_FILE_BLUE = COLOR_FILE + "/kcal_blue";
+
+    private static final String COLOR_FILE_SAT = COLOR_FILE + "/kcal_sat";
+    private static final String COLOR_FILE_HUE = COLOR_FILE + "/kcal_hue";
+    private static final String COLOR_FILE_VAL = COLOR_FILE + "/kcal_val";
+    private static final String COLOR_FILE_CONT = COLOR_FILE + "/kcal_cont";
+
 
     private boolean mUnsupported = false;
 
-    private SharedPreferences mPrefs;
 
     private ListPreference mXiaomiProfilePreference;
     private static String mXiaomiProfile;
-    private SeekBarPreference mColorTemp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -87,10 +118,37 @@ public class XiaomiDisplay extends PreferenceActivity implements
         mXiaomiProfilePreference.setValue(mXiaomiProfile);
         mXiaomiProfilePreference.setOnPreferenceChangeListener(this);
 
-        mColorTemp = (SeekBarPreference) findPreference(COLOR_TEMP);
-        mColorTemp.setInitValue(mPrefs.getInt(COLOR_TEMP, mColorTemp.def));
-        mColorTemp.setOnPreferenceChangeListener(this);
+        mKcalRed = (SeekBarPreference) findPreference(KEY_KCAL_RED);
+        mKcalRed.setInitValue(mPrefs.getInt(KEY_KCAL_RED, mKcalRed.def));
+        mKcalRed.setOnPreferenceChangeListener(this);
 
+        mKcalGreen = (SeekBarPreference) findPreference(KEY_KCAL_GREEN);
+        mKcalGreen.setInitValue(mPrefs.getInt(KEY_KCAL_GREEN, mKcalGreen.def));
+        mKcalGreen.setOnPreferenceChangeListener(this);
+
+        mKcalBlue = (SeekBarPreference) findPreference(KEY_KCAL_BLUE);
+        mKcalBlue.setInitValue(mPrefs.getInt(KEY_KCAL_BLUE, mKcalBlue.def));
+        mKcalBlue.setOnPreferenceChangeListener(this);
+
+        mKcalSaturation = (SeekBarPreference) findPreference(KEY_KCAL_SATURATION);
+        mKcalSaturation.setInitValue(mPrefs.getInt(KEY_KCAL_SATURATION, mKcalSaturation.def));
+        mKcalSaturation.setOnPreferenceChangeListener(this);
+
+        mKcalContrast = (SeekBarPreference) findPreference(KEY_KCAL_CONTRAST);
+        mKcalContrast.setInitValue(mPrefs.getInt(KEY_KCAL_CONTRAST, mKcalContrast.def));
+        mKcalContrast.setOnPreferenceChangeListener(this);
+
+        mKcalValue = (SeekBarPreference) findPreference(KEY_KCAL_VALUE);
+        mKcalValue.setInitValue(mPrefs.getInt(KEY_KCAL_VALUE, mKcalValue.def));
+        mKcalValue.setOnPreferenceChangeListener(this);
+
+        mKcalHue = (SeekBarPreference) findPreference(KEY_KCAL_HUE);
+        mKcalHue.setInitValue(mPrefs.getInt(KEY_KCAL_HUE, mKcalHue.def));
+        mKcalHue.setOnPreferenceChangeListener(this);
+
+        mKcalEyecare = (SeekBarPreference) findPreference(KEY_KCAL_EYECARE);
+        mKcalEyecare.setInitValue(mPrefs.getInt(KEY_KCAL_EYECARE, mKcalEyecare.def));
+        mKcalEyecare.setOnPreferenceChangeListener(this);
 
     }
 
@@ -99,43 +157,233 @@ public class XiaomiDisplay extends PreferenceActivity implements
     }
 
     public static void restore(Context context) {
-        try {
             String profile = PreferenceManager
                 .getDefaultSharedPreferences(context).getString(XiaomiDisplay.PREF_XIAOMI_PROFILE, DEFAULT_XIAOMI_PROFILE);
-            runCommand("display" + " " + profile);
+		       int col = Integer.parseInt(profile);
+          if (col == 1) {
+             runCommand("display ADAPT 1");
+         } else if(col == 2) {
+             runCommand("display ADAPT 2");
+         } else if (col == 3) {
+             runCommand("display ADAPT 3");
+         } else if (col == 4) {
+             runCommand("display ENHANCE 1");
+         } else if (col == 5) {
+            runCommand("display STANDARD 1");
+         } else if (col == 6) {
+           int storedRed = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_RED, 256);
+           int storedGreen = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_GREEN, 256);
+           int storedBlue = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_BLUE, 256);
+           int storedSaturation = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_SATURATION, 255);
+           int storedContrast = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_CONTRAST, 255);
+           int storedValue = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_VALUE, 255);
+           int storedHue = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_HUE, 0);
+           Utils.writeValue(COLOR_FILE_RED, String.valueOf(storedRed));
+           Utils.writeValue(COLOR_FILE_GREEN, String.valueOf(storedGreen));
+           Utils.writeValue(COLOR_FILE_BLUE, String.valueOf(storedBlue));
+           Utils.writeValue(COLOR_FILE_SAT, String.valueOf(storedSaturation));
+           Utils.writeValue(COLOR_FILE_CONT, String.valueOf(storedContrast));
+           Utils.writeValue(COLOR_FILE_VAL, String.valueOf(storedValue));
+           Utils.writeValue(COLOR_FILE_HUE, String.valueOf(storedHue));
+           runCommand("display ADAPT 3");
+         }  else if (col == 7) {
+           int storedeyecare = PreferenceManager
+                   .getDefaultSharedPreferences(context).getInt(XiaomiDisplay.KEY_KCAL_EYECARE, 10);
+            runCommand("display" + " " + "EYECARE" + " " + String.valueOf(storedeyecare));
+         }
 
-        } catch(Exception e) {
-        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.kcal_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.menu_reset:
+                reset();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void reset() {
+        int red = mKcalRed.reset();
+        int green = mKcalGreen.reset();
+        int blue = mKcalBlue.reset();
+        int saturation = mKcalSaturation.reset();
+        int contrast = mKcalContrast.reset();
+        int value = mKcalValue.reset();
+        int hue = mKcalHue.reset();
+        int eyecare = mKcalEyecare.reset();
+
+        mPrefs.edit().putInt(KEY_KCAL_RED, red).commit();
+        mPrefs.edit().putInt(KEY_KCAL_GREEN, green).commit();
+        mPrefs.edit().putInt(KEY_KCAL_BLUE, blue).commit();
+        mPrefs.edit().putInt(KEY_KCAL_SATURATION, saturation).commit();
+        mPrefs.edit().putInt(KEY_KCAL_CONTRAST, contrast).commit();
+        mPrefs.edit().putInt(KEY_KCAL_VALUE, value).commit();
+        mPrefs.edit().putInt(KEY_KCAL_HUE, hue).commit();
+        mPrefs.edit().putInt(KEY_KCAL_EYECARE, eyecare).commit();
+        Utils.writeValue(COLOR_FILE_RED, Integer.toString(red));
+        Utils.writeValue(COLOR_FILE_GREEN, Integer.toString(green));
+        Utils.writeValue(COLOR_FILE_BLUE, Integer.toString(blue));
+        Utils.writeValue(COLOR_FILE_SAT, Integer.toString(saturation));
+        Utils.writeValue(COLOR_FILE_CONT, Integer.toString(contrast));
+        Utils.writeValue(COLOR_FILE_VAL, Integer.toString(value));
+        Utils.writeValue(COLOR_FILE_HUE, Integer.toString(hue));
+        runCommand("display ADAPT 3");
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mXiaomiProfilePreference) {
                 mXiaomiProfile = newValue.toString();
                 mPrefs.edit().putString(PREF_XIAOMI_PROFILE, mXiaomiProfile).commit();
-                runCommand("display ENHANCE 1");
-                setCurrentProfile();
+                Utils.writeValue(COLOR_FILE_RED, Integer.toString(256));
+                Utils.writeValue(COLOR_FILE_GREEN, Integer.toString(256));
+                Utils.writeValue(COLOR_FILE_BLUE, Integer.toString(256));
+                Utils.writeValue(COLOR_FILE_SAT, Integer.toString(255));
+                Utils.writeValue(COLOR_FILE_CONT, Integer.toString(255));
+                Utils.writeValue(COLOR_FILE_VAL, Integer.toString(255));
+                Utils.writeValue(COLOR_FILE_HUE, Integer.toString(0));
+                mKcalRed.setEnabled(false);
+                mKcalGreen.setEnabled(false);
+          	mKcalBlue.setEnabled(false);
+      	    	mKcalSaturation.setEnabled(false);
+	       	mKcalContrast.setEnabled(false);
+	      	mKcalValue.setEnabled(false);
+	      	mKcalHue.setEnabled(false);
+	      	mKcalEyecare.setEnabled(false);
+                updateColorOptions(mXiaomiProfile);
+                return true;
+        }  else if (preference == mKcalRed) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_RED, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_RED, strVal);
+            runCommand("display ADAPT 3");
             return true;
-        } else if (preference == mColorTemp) {
-          float val = Float.parseFloat((String) newValue);
-           mPrefs.edit().putInt(COLOR_TEMP, (int) val).commit();
-           String strVal = ((String) newValue);
-           runCommand("display" + " " + "ADAPT" + " " + strVal);
-      return true;
-      }
+        } else if (preference == mKcalBlue) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_BLUE, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_BLUE, strVal);
+            runCommand("display ADAPT 3");
+            return true;
+        } else if (preference == mKcalGreen) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_GREEN, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_GREEN, strVal);
+            runCommand("display ADAPT 3");
+            return true;
+        } else if (preference == mKcalSaturation) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_SATURATION, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_SAT, strVal);
+            runCommand("display ADAPT 3");
+            return true;
+        } else if (preference == mKcalContrast) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_CONTRAST, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_CONT, strVal);
+            runCommand("display ADAPT 3");
+            return true;
+        } else if (preference == mKcalHue) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_HUE, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_HUE, strVal);
+            runCommand("display ADAPT 3");
+            return true;
+        } else if (preference == mKcalValue) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_VALUE, (int) val).commit();
+            String strVal = (String) newValue;
+            Utils.writeValue(COLOR_FILE_VAL, strVal);
+            runCommand("display ADAPT 3");
+            return true;
+        } else if (preference == mKcalEyecare) {
+            float val = Float.parseFloat((String) newValue);
+            mPrefs.edit().putInt(KEY_KCAL_EYECARE, (int) val).commit();
+            String strVal = (String) newValue;
+            runCommand("display" + " " + "EYECARE" + " " + strVal);
+            return true;
+        }
       return false;
   }
 
     private static void setCurrentProfile() {
         runCommand("display" + " " + mXiaomiProfile);
-    }
-	
-    private static void runCommand(String command) {
+   }
+
+   private static void runCommand(String command) {
         Log.d(TAG, "setCurrentProfile() cmd=\'" + command + "\'");
         ArrayList<String> output = Shell.runWithShell(command);
         for (String line : output) {
             Log.d(TAG, "setCurrentProfile() output:" + line);
         }
     }
+
+    private void updateColorOptions(String value) {
+          int temp = Integer.parseInt(value);
+          if (temp == 1) {
+             runCommand("display ADAPT 1");
+         } else if(temp == 2) {
+             runCommand("display ADAPT 2");
+         } else if (temp == 3) {
+             runCommand("display ADAPT 3");
+         } else if (temp == 4) {
+             runCommand("display ENHANCE 1");
+         } else if (temp == 5) {
+            runCommand("display STANDARD 1");
+         } else if (temp == 6) {
+            mKcalRed.setEnabled(true);
+            mKcalGreen.setEnabled(true);
+            mKcalBlue.setEnabled(true);
+      	    mKcalSaturation.setEnabled(true);
+	    mKcalContrast.setEnabled(true);
+	    mKcalValue.setEnabled(true);
+	    mKcalHue.setEnabled(true);
+           String mRed = String.valueOf(mPrefs.getInt(KEY_KCAL_RED, 256));
+           String mGreen = String.valueOf(mPrefs.getInt(KEY_KCAL_GREEN, 256));
+           String mBlue = String.valueOf(mPrefs.getInt(KEY_KCAL_BLUE, 256));
+           String mSaturation = String.valueOf(mPrefs.getInt(KEY_KCAL_SATURATION, 255));
+           String mContrast = String.valueOf(mPrefs.getInt(KEY_KCAL_CONTRAST, 255));
+           String mHue = String.valueOf(mPrefs.getInt(KEY_KCAL_HUE, 255));
+           String mValue = String.valueOf(mPrefs.getInt(KEY_KCAL_VALUE, 0));
+    	   Utils.writeValue(COLOR_FILE_RED, String.valueOf(mRed));
+           Utils.writeValue(COLOR_FILE_GREEN, String.valueOf(mGreen));
+           Utils.writeValue(COLOR_FILE_BLUE, String.valueOf(mBlue));
+           Utils.writeValue(COLOR_FILE_SAT, String.valueOf(mSaturation));
+           Utils.writeValue(COLOR_FILE_CONT, String.valueOf(mContrast));
+           Utils.writeValue(COLOR_FILE_VAL, String.valueOf(mValue));
+           Utils.writeValue(COLOR_FILE_HUE, String.valueOf(mHue));
+            runCommand("display ADAPT 3");
+         } else if (temp == 7) {
+  		     mKcalEyecare.setEnabled(true);
+           String mEyecare = String.valueOf(mPrefs.getInt(KEY_KCAL_EYECARE, 10));
+            runCommand("display" + " " + "EYECARE" + " " + String.valueOf(mEyecare));
+         }
+
+     }
 }
