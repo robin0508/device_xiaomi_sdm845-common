c8636ef81a5da987478c1442357e317c550d54aa
shivatejapeddi <shivatejapeddi@gmail.com>
2020-11-05 11:23:37 +0530

    sdm845-common : build CAF powerhal

--- sdm845.mk
+++ sdm845.mk
@@ -291,7 +291,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service \
+    android.hardware.power-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
